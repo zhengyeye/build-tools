d371cc4ba3265ab6e5184b3574d57afed90af9a2
zhengyeye <zhengyeye@ganggu.com>
2018-08-23 17:05:35 +0800

    增加grunt 工具

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # build-tools
+* grunt-test
+    * 实践中使用grunt demo
+    * 中文主页 : http://www.gruntjs.net/
+    * 是一套前端**自动化构建**工具，一个基于nodeJs的命令行工具
+    * 它是一个**任务运行器**, 配合其丰富强大的**插件**
+    * 常用功能:
+    * **合并文件**(js/css)
+    * **压缩文件**(js/css)
+    * **语法检查**(js/css)
+    * **less/sass预编译处理** 
+* 基于nodejs，因此需要安装nodejs
+```
+node -v       //查看版本号
+```
+* 目录结构
+  ```
+  |- build----------构建生成的文件所在的文件夹
+      |-css---------压缩后的css文件夹
+      |-js----------压缩后的js文件夹
+  |- css-------------css源码文件夹
+  |- src------------js源码文件夹   
+      |- test1.js---------------test1.js源文件夹,测试js文件
+      |- test2.js---------------test2.js源文件夹,测试js文件
+  |- .csslintrc-----检查css语法错误
+  |- .jshintrc-----检查javascript语法错误
+  |- index.html-----页面文件
+  |- Gruntfile.js---grunt配置文件(注意首字母大写)
+  |- package.json---项目包配置文件
+      {
+        "name": "grunt-test",
+        "version": "1.0.0"   
+      }
+  ``` 
+* Grunt插件介绍
+  * grunt官网的插件列表页面 http://www.gruntjs.net/plugins 
+  * 插件分类:
+    * grunt团队贡献的插件 : 插件名大都以contrib-开头
+    * 第三方提供的插件 : 大都不以contrib-开头
+  * 常用的插件:
+    * grunt-contrib-clean——清除文件(打包处理生成的)
+    * grunt-contrib-concat——合并多个文件的代码到一个文件中
+    * grunt-contrib-uglify——压缩js文件
+    * grunt-contrib-jshint——javascript语法错误检查；
+    * grunt-contrib-cssmin——压缩/合并css文件
+    * grunt-contrib-htmlmin——压缩html文件
+    * grunt-contrib-imagemin——压缩图片文件(无损)
+    * grunt-contrib-copy——复制文件、文件夹
+    * **grunt-contrib-requirejs**——合并压缩requirejs管理的所有js模块文件
+    * grunt-contrib-watch——实时监控文件变化、调用相应的任务重新执行
+
---
+++ grunt-test/Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+      //在Gruntfile.js中获取package.json中的内容
+      pkg: grunt.file.readJSON('package.json'),
+      //合并文件
+      concat: {
+        options: { //可选项配置
+          separator: ';'   //使用;连接合并
+        },
+        build: { //此名称任意
+          src:  ["src/*.js"],  //合并哪些js文件
+          dest: "build/js/built.js" //输出的js文件
+        }
+      },
+      //压缩js文件
+      uglify: {
+        options: {  //不是必须的
+            banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %> */'
+          },
+          build: {
+            files: {
+            //   'build/js/built-<%=pkg.name%>-<%=pkg.version%>.min.js': ['src/test1.js']
+              'build/js/built-<%=pkg.name%>-<%=pkg.version%>.min.js': ['build/js/built.js']
+            }
+          } 
+      },
+      //js语法检查： 使用jshint插件
+      jshint : {
+        options: {
+          jshintrc : '.jshintrc' //指定配置文件
+        },
+        // build : ['Gruntfile.js', 'src/*.js'] //指定检查的文件
+        test1:['Gruntfile.js'],
+        test2:['src/*.js']  
+      },
+      //压缩css文件
+      cssmin:{
+        options: {
+          shorthandCompacting: false,
+          roundingPrecision: -1
+        },
+        build: {
+          files: {//在build/css/下生成output.min.css
+              'build/css/output.min.css': ['css/*.css']
+          }
+        }
+      },
+      //檢查css語法錯誤
+      csslint:{
+        options: {
+            jshintrc : '.csslintrc' //指定配置文件
+          },
+          build : ['Gruntfile.js', 'css/*.css'] //指定检查的文件
+      },
+      //TODO:自动监听好像没用
+      watch : {
+        scripts : {
+          files : ['src/js/*.js', 'src/css/*.css'],
+          tasks : ['jshint','concat',  'uglify', 'cssmin'],
+          options : {spawn : false}  
+        }
+      }
+    });
+  
+     // 加载包含 "uglify" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['concat', 'uglify', 'jshint', 'watch']);
+    
+  
+};
